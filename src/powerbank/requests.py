ada1c8e70d145320c2e0a6158f1a256cb0653747
Olivier Dupouy <odupouy@forsk.com>
2024-02-08 14:10:16 +0100

    adding flake8 and pytest

--- src/powerbank/requests.py
+++ src/powerbank/requests.py
@@ -2,7 +2,14 @@ from requests import Session as BaseSession
 
 
 class Session(BaseSession):
-    def __init__(self, *args, base_url=None, token=None, token_prefix='bearer ', **kwargs) -> None:
+    def __init__(
+            self,
+            *args,
+            base_url=None,
+            token=None,
+            token_prefix='bearer ',
+            **kwargs
+    ) -> None:
         super().__init__(*args, **kwargs)
         self.base_url = base_url
         if token:
